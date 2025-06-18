cd0635fe84ac8e361ba64350de8fd0b8407a3f5c
kurieito0001@gmail.com <kurieito0001@gmail.com>
2025-06-18 22:51:43 +0500

    fix9

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PhoneInput from 'react-phone-number-input/input'
 import './App.css'
 import Header from './widgets/header/Header'
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Main from './widgets/main/Main';
+import Main from './widgets/main/Main'
 import './assets/styles/media.css'
-import Footer from './widgets/Footer/Footer';
+import Footer from './widgets/Footer/Footer'
 
+const App = () => {
+  const [loading, setLoading] = useState(true)
 
-  const App=()=> {
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false)
+    }
 
+    window.addEventListener('load', handleLoad)
+
+    return () => window.removeEventListener('load', handleLoad)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="loader__wrapper">
+        <div className='loader'></div>
+      </div>
+      
+    )
+  }
 
   return (
     <>
@@ -20,4 +38,4 @@ import Footer from './widgets/Footer/Footer';
   )
 }
 
-export default App;
+export default App
