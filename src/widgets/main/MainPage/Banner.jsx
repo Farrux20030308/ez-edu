a9f001d239f8c9e07b99e1a1c37f1eed67b22def
kurieito0001@gmail.com <kurieito0001@gmail.com>
2025-06-18 12:57:52 +0500

    fix07

--- src/widgets/main/MainPage/Banner.jsx
+++ src/widgets/main/MainPage/Banner.jsx
@@ -6,7 +6,10 @@ import Form from './Form/form';
         return(
         <div className='banner__wrapper'>
             <div className="banner container">
-                <h2 className='banner__text'>Английский - онлайн, просто и удобно</h2>               
+                <div className="banner__left">
+                <h2 className='banner__text'>Английский - онлайн, просто и удобно</h2> 
+                <video className='banner__video' src=""> <h3>EZ education - проще, чем кажется</h3></video>              
+                </div>
                 <Form />
             </div>
         </div>
